37f8519a942a6f437f13054c2b8dfd9d839e96ea
Otto Hockel <hockel.otto@googlemail.com>
2014-10-21 23:04:53 +0200

    updated metadata handling for metalsmith 1.0.1

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
-var assertDir = require('assert-dir-equal')
-var fingerprint = require('../')
-var Metalsmith = require('metalsmith')
+var assertDir = require('assert-dir-equal');
+var fingerprint = require('../');
+var Metalsmith = require('metalsmith');
+require('should');
 
 describe('metalsmith-fingerprint', function(){
 
@@ -8,7 +9,8 @@ describe('metalsmith-fingerprint', function(){
         Metalsmith('test/fixtures/basic')
             .use(fingerprint({ pattern: 'hello.txt' }))
             .use(function(files, metalsmith, done){
-                metalsmith.metadata().fingerprint['hello.txt'].should.equal('hello-22fd426562ac8a95a381a563d22ed04d.txt')
+                console.log(metalsmith.metadata().fingerprint['hello.txt']);
+                metalsmith.metadata().fingerprint['hello.txt'].should.equal('hello-22fd426562ac8a95a381a563d22ed04d.txt');
                 return process.nextTick(done)
             })
             .build(function(err){
