8b5b2f65049d7462579d1d564f5099ec85f90827
Christopher Cliff <chris@percolate.com>
2014-10-22 09:58:21 -0500

    Cleanup.

--- test/index.js
+++ test/index.js
@@ -1,16 +1,14 @@
-var assertDir = require('assert-dir-equal');
-var fingerprint = require('../');
-var Metalsmith = require('metalsmith');
-require('should');
+var assertDir = require('assert-dir-equal')
+var fingerprint = require('../')
+var Metalsmith = require('metalsmith')
 
 describe('metalsmith-fingerprint', function(){
 
     it('should fingerprint the file', function(done){
-        Metalsmith('test/fixtures/basic')
+        (new Metalsmith('test/fixtures/basic'))
             .use(fingerprint({ pattern: 'hello.txt' }))
             .use(function(files, metalsmith, done){
-                console.log(metalsmith.metadata().fingerprint['hello.txt']);
-                metalsmith.metadata().fingerprint['hello.txt'].should.equal('hello-22fd426562ac8a95a381a563d22ed04d.txt');
+                metalsmith.metadata().fingerprint['hello.txt'].should.equal('hello-22fd426562ac8a95a381a563d22ed04d.txt')
                 return process.nextTick(done)
             })
             .build(function(err){
